6c09b9ae99d99cb650193648eb22370102b05311
Barrett Ruth <br.barrettruth@gmail.com>
2025-09-18 19:54:52 -0400

    fix(ci): format

--- tests/minimal.lua
+++ tests/minimal.lua
@@ -1,4 +1,4 @@
-vim.env.XDG_CONFIG_HOME = vim.fn.getcwd() .. '/.tests/xdg/config'
-vim.env.XDG_DATA_HOME = vim.fn.getcwd() .. '/.tests/xdg/data'
-vim.env.XDG_STATE_HOME = vim.fn.getcwd() .. '/.tests/xdg/state'
+vim.env.XDG_CONFIG_HOME = vim.fn.getcwd() .. "/.tests/xdg/config"
+vim.env.XDG_DATA_HOME = vim.fn.getcwd() .. "/.tests/xdg/data"
+vim.env.XDG_STATE_HOME = vim.fn.getcwd() .. "/.tests/xdg/state"
 vim.opt.runtimepath:append(vim.fn.getcwd())
