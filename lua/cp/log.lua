18dbcd43d2adfa393ae3cf78cbbe510bef08c9d6
Barrett Ruth <br.barrettruth@gmail.com>
2025-10-04 12:48:57 -0400

    fix(cache): contest override

--- lua/cp/log.lua
+++ lua/cp/log.lua
@@ -1,8 +1,9 @@
 local M = {}
 
 function M.log(msg, level, override)
+  local debug = require('cp.config').get_config().debug or false
   level = level or vim.log.levels.INFO
-  if level >= vim.log.levels.WARN or override then
+  if level >= vim.log.levels.WARN or override or debug then
     vim.schedule(function()
       vim.notify(('[cp.nvim]: %s'):format(msg), level)
     end)
